4cf07a437ae02f5d2ebf3c3c8f13ae47fe6bffcb
Abhirup Nandy <abhirupnandy@gmail.com>
2020-07-13 22:13:44 +0530

    writeup + search + login left

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
     style="min-height: 80px">
         <div class="row pt-3">
             <div class="col-12 col-sm offset-sm-2">
-                <a type="button" href="#" class="link">
+                <a type="button" href="#" class="link" data-toggle="modal" data-target="#termsModal">
                     <span class="link">Terms of Use</span></a>
             </div>
             <div class="col-12 col-sm">
-                <a type="button" href="#" class="link">
+                <a type="button" href="#" class="link" data-toggle="modal" data-target="#aboutModal">
                     <span class="link">About the Portal</span></a>
             </div>
             <div class="col-12 col-sm-5 copyright">
@@ -66,11 +66,90 @@
                     </form>
                     -->
                 </div>
+                
             </div>
         </div>
     </div>
-    
-    <!-- Scripts -->
+<!-- Terms of Use -->
+<div id="termsModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-md">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h4 class="modal-title">Terms of Use </h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span style="color: snow;">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h1 class="text-center text-danger font-weight-bolder py-5">Page under Construction</h1>
+                <h3 class="text-center text-success font-weight-bolder py-5">Please wait</h3>
+                <!--
+				<form>
+					<div class="form-group col-sm-auto">
+						<label class="sr-only" for="exampleInputEmail3">Email address</label>
+						<input type="email" class="form-control form-control-sm mr-1" id="exampleInputEmail3"
+							placeholder="Enter email">
+					</div>
+					<div class="form-group col-sm-auto">
+						<label class="sr-only" for="exampleInputPassword3">Password</label>
+						<input type="password" class="form-control form-control-sm mr-1" id="exampleInputPassword3"
+							placeholder="Password">
+					</div>
+					<div class="col-sm-auto">
+						<div class="form-check">
+							<div class="row">
+								<div class="col-2 col-sm-1 pt-2" >
+									<input class="form-check-input" type="checkbox">
+								</div>
+								<div class="col col-sm">
+									<label class="form-check-label"> Remember me</label>
+								</div>
+							</div>
+							
+							
+						</div>
+					</div>
+					<br>
+					<div class="form-row">
+						<button type="button" class="btn btn-secondary btn-md mr-auto"
+							data-dismiss="modal">Cancel</button>
+						<button type="submit" class="btn btn-dark btn-md mr-1">Sign in</button>
+					</div>
+				</form>
+				-->
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-dark btn-sm" data-dismiss="modal" aria-hidden="true">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- About us -->
+<div id="aboutModal" class="modal fade" role="dialog">
+    <div class="modal-dialog modal-md">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header bg-dark">
+                <h4 class="modal-title">About Us </h4>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span style="color: snow;">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h5 class="h5 text-center text-secondary">The Indian Science Research Group</h5>
+                
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-dark btn-sm" data-dismiss="modal" aria-hidden="true">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Scripts -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS. -->
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.min.js"></script>
